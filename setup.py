b17c7765ce4d81c2458de5b4c5280020ffa54f1d
Zhong Lizhi <bliss@3zso.com>
2024-10-12 16:03:50 +0800

    Update setup.py for dify-api-python package
    - Set package version to 0.1.1
    - Add author information and project URL
    - Specify package structure and dependencies
    - Include classifiers for Python versions and development status
    - Set minimum Python version requirement to 3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dify-api-python",
-    version="0.1.0",
+    version="0.1.1",
     author="zlz3907",
     author_email="zlz3907@gmail.com",
     description="A Python client for the Dify API",
